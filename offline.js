bb2143412ce71e523c13e8416e4bde1612474078
Bitowsky <67092848+Bitowsky@users.noreply.github.com>
2021-11-14 19:07:35 +0100

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1636913034,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/platform-sheet0.png",
+		"images/player-sheet0.png",
+		"images/trash-sheet0.png",
+		"images/player2-sheet0.png",
+		"images/player2-sheet1.png",
+		"images/base-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"eventcodes.json",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
